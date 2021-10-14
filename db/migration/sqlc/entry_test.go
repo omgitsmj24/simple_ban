da6c577d9f6f626bfcd3efaae1386fea230af6b7
HOANG TRAN HUY <HOANG.TH182953@sis.hust.edu.vn>
2021-10-15 00:28:01 +0700

    first commit

---
+++ db/migration/sqlc/entry_test.go
@@ -0,0 +1,70 @@
+package db
+
+import (
+	"context"
+	"simplebank/util"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func CreateRandomEntry(t *testing.T, account1 Account) Entry {
+	arg := CreateEntryParams{
+		AccountID: account1.ID,
+		Amount:    util.RandomMoney(),
+	}
+
+	entry, err := testQueries.CreateEntry(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, entry)
+
+	require.Equal(t, arg.AccountID, entry.AccountID)
+	require.Equal(t, arg.Amount, entry.Amount)
+
+	require.NotZero(t, entry.ID)
+	require.NotZero(t, entry.CreatedAt)
+
+	return entry
+}
+
+func TestCreateEntry(t *testing.T) {
+	account1 := createRandomAccount(t)
+	CreateRandomEntry(t, account1)
+}
+
+func TestGetEntry(t *testing.T) {
+	account1 := createRandomAccount(t)
+	entry1 := CreateRandomEntry(t, account1)
+
+	entry2, err := testQueries.GetEntry(context.Background(), entry1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, entry2)
+
+	require.Equal(t, entry1.AccountID, entry2.AccountID)
+	require.Equal(t, entry1.Amount, entry2.Amount)
+
+	require.WithinDuration(t, entry1.CreatedAt, entry2.CreatedAt, time.Second)
+}
+
+func TestListEntries(t *testing.T) {
+	account1 := createRandomAccount(t)
+	for i := 0; i < 10; i++ {
+		CreateRandomEntry(t, account1)
+	}
+
+	arg := ListEntriesParams{
+		AccountID: account1.ID,
+		Limit:     5,
+		Offset:    5,
+	}
+
+	entries, err := testQueries.ListEntries(context.Background(), arg)
+	require.NoError(t, err)
+	require.Len(t, entries, 5)
+
+	for _, entry := range entries {
+		require.NotZero(t, entry)
+		require.Equal(t, arg.AccountID, entry.AccountID)
+	}
+}
